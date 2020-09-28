a47a77e5d64e66ba4f3e7c942a27cbc0ac667bdc
gabrielajimpikit <ljimpikitc@est.ups.edu.ec>
2020-09-28 07:10:56 +0100

    actualizando la clase

---
+++ code/node-fundamentals/public/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const response = require('./network/response')
+
+const router = express.Router()
+
+var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(router)
+
+router.get('/message', function(req, res) {
+    console.log( req.headers )
+    res.header({
+        "custom-header":"Nuestro valor personalizado",
+    })
+    response.success(req, res, 'Lista de mensajes.')
+})
+
+router.post('/message', function(req, res) {
+    console.log( req.query )
+    if (req.query.error == 'ok') {
+        response.error(req, res, 'Error simulado.', 500, 'Es solo una simulacion de los errores.')
+    } else {
+        response.success(req, res, 'Creado correctamente.', 201)
+    }
+})
+
+app.use('/app', express.static('public'))
+
+app.listen(3000)
+console.log('La aplicación está escuchando en http://localhost:3000')
