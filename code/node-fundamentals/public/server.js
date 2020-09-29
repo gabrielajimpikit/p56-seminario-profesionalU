0cfafb95130fe72eb1355265028955206070d225
gabrielajimpikit <ljimpikitc@est.ups.edu.ec>
2020-09-29 06:23:34 +0100

    actualizando

--- code/node-fundamentals/public/server.js
+++ code/node-fundamentals/public/server.js
@@ -5,28 +5,22 @@ const response = require('./network/response')
 const router = express.Router()
 
 var app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(router)
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded( {extended:false} ) ) 
+app.use( router )
 
-router.get('/message', function(req, res) {
-    console.log( req.headers )
-    res.header({
-        "custom-header":"Nuestro valor personalizado",
-    })
-    response.success(req, res, 'Lista de mensajes.')
+router.get('/carrera', function(req, res) {
+    response.success( req, res, 'Lista de Carreras de la UPS.', 200 ) 
 })
-
-router.post('/message', function(req, res) {
-    console.log( req.query )
+router.post('/carrera', function(req, res) {
     if (req.query.error == 'ok') {
-        response.error(req, res, 'Error simulado.', 500, 'Es solo una simulacion de los errores.')
+        response.error( req, res, 'Error al ingresar la Carrera.', 500 )        
     } else {
-        response.success(req, res, 'Creado correctamente.', 201)
+        response.success( req, res, 'Ingreso de Carrera exitoso.', 201 )        
     }
 })
 
-app.use('/app', express.static('public'))
+app.use( '/', express.static('public') )
 
-app.listen(3000)
-console.log('La aplicación está escuchando en http://localhost:3000')
+app.listen( 5000 )
+console.log( 'La aplicación está escuchando en http://localhost:5000' )
